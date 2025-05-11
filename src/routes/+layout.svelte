095ebbefe989376b65af2fea10a4915e737a7650
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-05-11 12:56:13 +0200

    added js enabled

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,17 @@
     import Header from "$lib/organisms/Header.svelte"
     import Footer from "$lib/organisms/Footer.svelte"
 
+    import { javascript } from "$lib/utils/javascriptEnabled.svelte.js"
+    import { onMount } from "svelte"
+
     let { children } = $props();
+
+    onMount(() => {
+        // Check if JavaScript is enabled
+        if (typeof window !== 'undefined' && typeof window.document !== 'undefined') {
+        javascript.enabled = true;
+        }
+    });
 </script>
 
 <svelte:head>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,27 @@
+<script>
+    import Portret from "$lib/molecules/Portret.svelte"
+</script>
+
 <main>
     <section class="hero">
         <h2>hier kom ik te staan</h2>
+        <Portret />
+    </section>
+    <section>
+        <h2>"Insert quote"</h2>
     </section>
     <section class="selected-work">
         <h2>Selected work</h2>
+        <a href="/projects">see more</a>
+        <ul>
+            <li>
+                <article>
+                    <h3>Title</h3>
+                    <time datetime="03-01-25">03-04-25</time>
+                    <p>* insert description *</p>
+                </article>
+            </li>
+        </ul>
     </section>
 </main>
 
