5e62682edad72910da5cd587fdf5d2128a93a4b1
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-05-11 23:57:44 +0200

    projectDetail: added some content

--- src/routes/[lang]/projects/[slug]/+page.svelte
+++ src/routes/[lang]/projects/[slug]/+page.svelte
@@ -6,5 +6,17 @@
 </script>
 
 <main>
-    hi
+    <h2>{project.title}</h2>
+    <small>{project.date}</small>
+    <p>{project.description}</p>
+    <p>{project.type}</p>
+    <img src="{project.coverImage.url}" alt="{project.title}" width="100" height="100">
+    <ul>
+        {#each project.content as content}
+            <li>
+                <h3>{content.title}</h3>
+                <p>{content.contentMarkdown}</p>
+            </li>
+        {/each}
+    </ul>
 </main>
