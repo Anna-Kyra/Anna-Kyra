73406f572ce24bbf05f1d99e5a95cc1462d9504e
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-05-11 23:04:52 +0200

    projects: link naar project werkt nu

--- src/routes/[lang]/projects/+page.svelte
+++ src/routes/[lang]/projects/+page.svelte
@@ -1,17 +1,21 @@
 <script>
+    import { page } from '$app/state';
     let { data } = $props()
-    let { page } = data.data
+    let pages = data.data.page
     let { projects } = data.data
+
+    console.log(page.url)
 </script>
 
 <main>
-    <h1>{page.title}</h1>
+    <h1>{pages.title}</h1>
     <ul>
 		{#each projects as project}
 			<li>
                 <h2>{project.title}</h2>
                 <p>{project.description}</p>
                 <img src="{project.coverImage.url}" alt="{project.title}" width="100" height="100">
+                <a href="{page.url.pathname}{project.slug}">{project.slug}</a>
             </li>
 		{/each}
 	</ul>
