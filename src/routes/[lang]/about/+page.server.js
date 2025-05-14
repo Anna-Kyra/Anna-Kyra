039aa7f7566dd3dea821b53f681b5de257135913
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-05-14 11:23:32 +0200

    Added querie on about page

---
+++ src/routes/[lang]/about/+page.server.js
@@ -0,0 +1,54 @@
+// src/routes/[lang]/+page.server.js
+import { gql } from 'graphql-request';
+import { hygraph } from '$lib/utils/hygraph.js';
+
+const query = gql`
+    query About($locale: Locale!) {
+        page(where: {id: "cmainlnvaop1u07mmkxpch5hj"}, locales: [$locale]) {
+            title
+            slug
+            description
+            image {
+                url
+                width
+                height
+            }
+            link {
+                linkTitle
+                url
+            }
+            video {
+                height
+                url
+                width
+            }
+        }
+        techStacks {
+            logo {
+                height
+                url
+                width
+            }
+            type
+            name
+        }
+    }
+`
+export async function load({ params }) {
+    const localeMap = {
+        nl: 'nl_NL',
+        en: 'en'
+    };
+
+    const lang = params.lang || 'en'
+    const locale = localeMap[lang] || 'en'
+
+    const slug = params.slug
+
+    const data = await hygraph.request(query, { slug, locale })
+
+    return {
+        data,
+        locale
+    };
+}
--- src/routes/[lang]/about/+page.svelte
+++ src/routes/[lang]/about/+page.svelte
@@ -1,3 +1,12 @@
+<script>
+    let { data } = $props()
+    let { page } = data.data
+    let { techStacks } = data.data
+
+
+    console.log(page, techStacks)
+</script>
+
 <main>
     <h1>About</h1>
 </main>
