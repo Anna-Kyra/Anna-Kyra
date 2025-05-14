beebfc5c32ce4736b37276a5aafe28581571fb30
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-05-14 10:59:22 +0200

    Added Contact query WIP

---
+++ src/routes/[lang]/contact/+page.server.js
@@ -0,0 +1,40 @@
+// src/routes/[lang]/+page.server.js
+import { gql } from 'graphql-request';
+import { hygraph } from '$lib/utils/hygraph.js';
+
+const query = gql`
+    query Contact($locale: Locale!) {
+        page(where: {id: "cmanp3f01ejxz07mh1883beaa"}, locales: [$locale]) {
+            title
+            slug
+            description
+            image {
+                url
+                width
+                height
+            }
+            link {
+                linkTitle
+                url
+            }
+        }
+    }
+`
+export async function load({ params }) {
+    const localeMap = {
+        nl: 'nl_NL',
+        en: 'en'
+    };
+
+    const lang = params.lang || 'en'
+    const locale = localeMap[lang] || 'en'
+
+    const slug = params.slug
+
+    const data = await hygraph.request(query, { slug, locale })
+
+    return {
+        data,
+        locale
+    };
+}
