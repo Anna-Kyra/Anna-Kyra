3bc570eb5342ea41116816e03c73b0b913f84300
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-05-16 09:33:16 +0200

    Added content on contact

--- src/routes/[lang]/contact/+page.svelte
+++ src/routes/[lang]/contact/+page.svelte
@@ -2,10 +2,16 @@
     let { data } = $props()
     let { page } = data.data
 
-    console.log(page)
+    console.log(page.image[0].url)
 </script>
 
 <main>
-    <h1>{page.title}</h1>
-    <p>{page.description}</p>
+    <section>
+        <h1>{page.title}</h1>
+        
+    </section>
+    <section>
+        <img src="{page.image[0].url}" alt="" width="{page.image.width}" height="{page.image.height}">
+        <p>{page.description}</p>
+    </section>
 </main>
