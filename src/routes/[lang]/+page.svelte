7f954096405f8d596e42de428dd39982c729b590
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-05-22 13:44:05 +0200

    layout homepage WIP

--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -10,29 +10,54 @@
 </script>
 
 <main>
-    <sections class="hero">
+    <section class="hero">
         <h1>{pageData.title}</h1>
         
         <Portret />
-    </sections>
-    <sections>
+    </section>
+    <section>
         <h2>{pageData.quote}</h2>
-    </sections>
-    <sections class="selected-work">
+    </section>
+    <section class="selected-work">
         <h2>{pageData.content[0].title}</h2>
-        {#each projects as project}
-			<li>
-                <img src="{project.coverImage.url}" alt="{project.title}" width="{project.coverImage.width}" height="{project.coverImage.height}">
-                <h2>{project.title}</h2>
-                <time datetime="{project.date}">{project.date}</time>
-                <p>{project.description}</p>
-                <p>{project.type}</p>
-                <a href="{page.url.pathname}projects/{project.slug}">Zie meer</a>
-            </li>
-		{/each}
-    </sections>
+        <ul>
+            {#each projects as project}
+                <li>
+                    <img src="{project.coverImage.url}" alt="{project.title}" width="{project.coverImage.width}" height="{project.coverImage.height}">
+                    <h2>{project.title}</h2>
+                    <time datetime="{project.date}">{project.date}</time>
+                    <p>{project.description}</p>
+                    <p>{project.type}</p>
+                    <a href="{page.url.pathname}projects/{project.slug}">Zie meer</a>
+                </li>
+            {/each}
+        </ul>
+
+    </section>
 </main>
 
 <style>
+    main {
+        display: flex;
+        flex-direction: column;
+    }
+    section {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 2rem;
 
+        padding: 2rem;
+        background-color: hotpink;
+    }
+
+    ul {
+        list-style: none;
+    }
+
+    .hero {
+        height: calc(100vh - 6rem);
+        justify-content: center;
+        background-color: aqua;
+    }
 </style>
