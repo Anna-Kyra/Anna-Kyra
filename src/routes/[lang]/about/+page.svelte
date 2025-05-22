05d09748c60571bf27e93190ddb32aa369660c90
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-05-22 14:10:36 +0200

    layout about WIP

--- src/routes/[lang]/about/+page.svelte
+++ src/routes/[lang]/about/+page.svelte
@@ -8,11 +8,36 @@
 </script>
 
 <main>
-    <h1>{page.title}</h1>
-    <p>{page.description}</p>
-    <ul>
-        {#each techStacks as techStack}
-            <li>{techStack.name}</li>
-        {/each}
-    </ul>
+    <section>
+        <h1>{page.title}</h1>
+    </section>
+    <section>
+        <img src="" alt="">
+        <div>
+            <h2>Anna-Kyra Strik</h2>
+            <p>{page.description}</p>
+        </div>
+    </section>
+    <section>
+        <h2>Programma kennis</h2>
+        <ul>
+            {#each techStacks as techStack}
+                <li>{techStack.name}</li>
+            {/each}
+        </ul>
+    </section>
 </main>
+
+<style>
+    section {
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+        padding: 2rem;
+        align-items: center;
+    }
+    section:nth-of-type(2) {
+        justify-content: center;
+        flex-direction: row;
+    }
+</style>
