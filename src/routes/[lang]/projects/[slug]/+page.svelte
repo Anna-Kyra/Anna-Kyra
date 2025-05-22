b142b370913b587beb5f01ac2842a33a40fe1198
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-05-22 13:59:56 +0200

    layout projects detail

--- src/routes/[lang]/projects/[slug]/+page.svelte
+++ src/routes/[lang]/projects/[slug]/+page.svelte
@@ -4,17 +4,42 @@
 </script>
 
 <main>
-    <h2>{project.title}</h2>
-    <small>{project.date}</small>
-    <p>{project.description}</p>
-    <p>{project.type}</p>
-    <img src="{project.coverImage.url}" alt="{project.title}" width="100" height="100">
-    <ul>
-        {#each project.content as content}
-            <li>
-                <h3>{content.title}</h3>
-                <p>{content.contentMarkdown}</p>
-            </li>
-        {/each}
-    </ul>
+    <section>
+        <img src="{project.coverImage.url}" alt="{project.title}" width="100" height="100">
+        <article>
+            <h2>{project.title}</h2>
+            <small>{project.date}</small>
+            <p>{project.description}</p>
+            <p>{project.type}</p>
+            <button>Read more</button>
+        </article>
+    </section>
+    <section class="content">
+        <ul>
+            {#each project.content as content}
+                <li>
+                    <h3>{content.title}</h3>
+                    <p>{content.contentMarkdown}</p>
+                </li>
+            {/each}
+        </ul>
+    </section>
 </main>
+
+<style>
+    section {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 2rem;
+
+        padding: 2rem;
+        background-color: hotpink;
+    }
+
+    section:nth-of-type(1) {
+        flex-direction: row;
+        justify-content: space-around;
+        min-height: 50vh;
+    }
+</style>
