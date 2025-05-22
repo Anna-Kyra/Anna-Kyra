ed9eca7bda723a834b598f2bafb5dbc0f5e68a4c
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-05-22 13:36:21 +0200

    basic styling footer

--- src/lib/organisms/Footer.svelte
+++ src/lib/organisms/Footer.svelte
@@ -6,12 +6,53 @@
 
 <footer>
     <h2>{footer.title}</h2>
-	<ul>
-		{#each social as social (social)}
-			<li><a target="_blank" href="{social.url}">{social.name}</a></li>
-		{/each}
-	</ul>
+
+	<section>
+		<!-- Navigatie -->
+		<article>
+			<h3>Navigation</h3>
+			<ul></ul>
+		</article>
+
+		<!-- Contact -->
+		<article>
+			<h3>Contact</h3>
+			<ul></ul>
+		</article>
+
+		<!-- Socials -->
+		<article>
+			<h3>Socials</h3>
+			<ul>
+				{#each social as social (social)}
+					<li><a target="_blank" href="{social.url}">{social.name}</a></li>
+				{/each}
+			</ul>
+		</article>
+
+		<!-- EYE -->
+	</section>
+
     <p>{footer.copyright}</p>
 
     {footer.social[0].id}
 </footer>
+
+<style>
+	footer {
+		display: flex;
+		flex-direction: column;
+		padding: 2rem;
+		background-color: goldenrod;
+	}
+
+	section {
+		display: flex;
+		gap: 1rem;
+		justify-content: space-between;
+	}
+
+	ul {
+		list-style: none;
+	}
+</style>
